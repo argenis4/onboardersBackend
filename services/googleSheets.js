5e24c4095deb1f429fdcbef22094a65165de1f2b
argenis4 <gonzalezargenis6@gmail.com>
2024-08-06 15:55:33 -0300

    Update googleSheets.js

--- services/googleSheets.js
+++ services/googleSheets.js
@@ -1,14 +1,14 @@
 const { google } = require('googleapis');
 const sheets = google.sheets('v4');
 const path = require('path');
-const { GOOGLE_URL } = require('../config.js'); 
+
 
 const credentialsPath = path.resolve(__dirname, '../testcloud-431621-5623f2ecb7b3.json'); // Configurar con las credenciales de la API de Google
 
 async function authenticate() {
   const auth = new google.auth.GoogleAuth({
    keyFile: credentialsPath,
-    scopes: [GOOGLE_URL],
+    scopes: ['https://www.googleapis.com/auth/spreadsheets],
   });
   return await auth.getClient();
 }
