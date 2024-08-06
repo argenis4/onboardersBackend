1d6698f6c22530aed600cc4bf151f2d0376c16a4
argenis4 <gonzalezargenis6@gmail.com>
2024-08-06 16:06:59 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
 export const GOOGLE_URL= "https://www.googleapis.com/auth/spreadsheets"
+const credentialsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS || path.resolve(__dirname, '../testcloud-431621-5623f2ecb7b3.json');
